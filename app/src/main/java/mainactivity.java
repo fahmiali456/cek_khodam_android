276454f5ac500ebab0e0fb20c4ee728becbd6d35
fahmiali4560 <fahmialimmudin@gmail.com>
2024-11-18 15:11:07 +0700

    upload file

---
+++ app/src/main/java/mainactivity.java
@@ -0,0 +1,17 @@
+public class mainactivity {package com.yourpackage;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // Tambahkan logika tambahan jika diperlukan, misalnya animasi atau pengalihan ke layar berikutnya
+        }
+    }
+
+}
---
+++ app/src/main/java/rumus.java
@@ -0,0 +1,2 @@
+public class rumus {
+}
